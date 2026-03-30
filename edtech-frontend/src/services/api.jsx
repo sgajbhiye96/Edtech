101057ba45c3f6f47de8eeb54fd440a0629a06de
Siddharth <sgajbhiye96@gmail.com>
2026-03-30 18:44:31 +0530

    bug fix

--- edtech-frontend/src/services/api.jsx
+++ edtech-frontend/src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
 
-  baseURL: import.meta.env.VITE_API_URL || "https://edtech-backend.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://edtech-backend-f7p4.onrender.com/api",
 
 });
 
