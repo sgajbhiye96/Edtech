3479c9f07d7ed4af878e48e4c15374c858fd5b80
Siddharth <sgajbhiye96@gmail.com>
2026-03-30 16:12:47 +0530

    bugs fix

--- edtech-frontend/src/context/AuthContext.jsx
+++ edtech-frontend/src/context/AuthContext.jsx
@@ -3,27 +3,45 @@ import { createContext, useState, useEffect } from "react";
 export const AuthContext = createContext();
 
 export default function AuthProvider({ children }) {
+
   const [user, setUser] = useState(null);
 
   useEffect(() => {
+
     const username = localStorage.getItem("username");
-    if (username) setUser(username);
+
+    const token = localStorage.getItem("token");
+
+    if (username && token) setUser(username);
+
   }, []);
 
   const login = (username, token) => {
+
     localStorage.setItem("username", username);
+
     localStorage.setItem("token", token);
+
     setUser(username);
+
   };
 
   const logout = () => {
-    localStorage.clear();
+
+    localStorage.removeItem("username");
+
+    localStorage.removeItem("token");
+
     setUser(null);
+
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+<AuthContext.Provider value={{ user, login, logout }}>
+
       {children}
-    </AuthContext.Provider>
+</AuthContext.Provider>
+
   );
+
 }
