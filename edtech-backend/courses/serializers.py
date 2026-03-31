c74466c2d4345f24ba609b096cbf2f144784a4d3
Siddharth <sgajbhiye96@gmail.com>
2026-03-31 13:42:41 +0530

    file updated

--- edtech-backend/courses/serializers.py
+++ edtech-backend/courses/serializers.py
@@ -1,16 +1,12 @@
 from rest_framework import serializers
 from .models import Course, Lesson
-
 class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = "__all__"
-
-
+   class Meta:
+       model = Lesson
+       fields = "__all__"
 class CourseSerializer(serializers.ModelSerializer):
-    thumbnail = serializers.ImageField(required=False)
-
-    class Meta:
-        model = Course
-        fields = "__all__"
-
+   thumbnail = serializers.ImageField(required=False)
+   lessons = LessonSerializer(many=True, read_only=True)  # ← add this
+   class Meta:
+       model = Course
+       fields = "__all__"
