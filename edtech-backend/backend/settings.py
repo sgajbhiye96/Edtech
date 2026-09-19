d538e4a6898b4a0530822d1ef5e958854da75a22
Shreeya Bhange <G688190@genmills.com>
2026-09-19 13:29:52 +0530

    Fix SECRET_KEY to read from environment

--- edtech-backend/backend/settings.py
+++ edtech-backend/backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 from django.core.management.utils import get_random_secret_key
 
-SECRET_KEY = get_random_secret_key()
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # ─── Security ─────────────────────────────────────────────
