1d89c721529568a2db7f6325b491370a690a425d
SIDDHARTH GAJBHIYE <72270797+sgajbhiye96@users.noreply.github.com>
2026-09-19 21:08:21 +0530

    Build student live learning dashboard

--- edtech-frontend/src/App.jsx
+++ edtech-frontend/src/App.jsx
@@ -1,89 +1,48 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import AuthProvider from "./context/AuthContext";
-
 import About from "./pages/About";
-
 import Navbar from "./components/Navbar";
-
 import ProtectedRoute from "./components/ProtectedRoute";
-
 import Login from "./pages/Login";
-
 import Register from "./pages/Register";
-
 import Courses from "./pages/Courses";
-
 import CourseDetails from "./pages/CourseDetails";
-
 import Dashboard from "./pages/Dashboard";
-
 import Home from "./pages/Home";
-
 import AdminLayout from "./admin/AdminLayout";
-
 import AdminLogin from "./admin/AdminLogin";
-
 import AdminDashboard from "./admin/Dashboard";
-
 import AdminCourses from "./admin/Courses";
-
 import AddCourse from "./admin/AddCourse";
-
 import Lessons from "./admin/Lessons";
-
 import Users from "./admin/Users";
-
 import Enrollments from "./admin/Enrollments";
-import './index.css';
+import "./index.css";
 
 export default function App() {
-
   return (
-<AuthProvider>
-<BrowserRouter>
-<Navbar />
-<Routes>
-
-          {/* PUBLIC ROUTES */}
-<Route path="/" element={<Home />} />
-<Route path="/courses" element={<Courses />} />
-<Route path="/courses/:id" element={<CourseDetails />} />
-<Route path="/login" element={<Login />} />
-<Route path="/register" element={<Register />} />
-<Route path="/about" element={<About />} />
-
-          {/* USER PROTECTED ROUTE */}
-<Route
-
-            path="/dashboard"
-
-            element={
-<ProtectedRoute>
-<Dashboard />
-</ProtectedRoute>
-
-            }
-
-          />
-
-          {/* ADMIN LOGIN - separate path ✅ */}
-<Route path="/admin/login" element={<AdminLogin />} />
-
-          {/* ADMIN PANEL */}
-<Route path="/admin" element={<AdminLayout />}>
-<Route path="dashboard" element={<AdminDashboard />} />
-<Route path="courses" element={<AdminCourses />} />
-<Route path="courses/add" element={<AddCourse />} />
-<Route path="courses/:id/lessons" element={<Lessons />} />
-<Route path="users" element={<Users />} />
-<Route path="enrollments" element={<Enrollments />} />
-</Route>
-</Routes>
-</BrowserRouter>
-</AuthProvider>
-
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/courses/:id" element={<CourseDetails />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route path="dashboard" element={<AdminDashboard />} />
+            <Route path="courses" element={<AdminCourses />} />
+            <Route path="courses/add" element={<AddCourse />} />
+            <Route path="courses/:id/lessons" element={<Lessons />} />
+            <Route path="users" element={<Users />} />
+            <Route path="enrollments" element={<Enrollments />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
-
 }
- 
